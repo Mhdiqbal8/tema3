866b3783f7215bc5540abe4bdf973d85766a76c1
mhdiqbal8 <muh.iqbal976@gmail.com>
2023-12-02 20:35:00 +0700

    fisrt commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 simplyCountdown(".simply-countdown", {
   year: 2023, // required
   month: 12, // required
-  day: -15, // required
+  day: 0, // required
   hours: 12, // Default is 0 [0-23] integer
   minutes: 0, // Default is 0 [0-59] integer
   seconds: 0, // Default is 0 [0-59] integer
@@ -28,46 +28,74 @@ simplyCountdown(".simply-countdown", {
 
 // open
 
+// document.getElementById("copyButton").addEventListener("click", function () {
+//   let nomor = this.getAttribute("data-nomer");
+//   let textArea = document.createElement("textarea");
+
+//   textArea.value = nomor;
+//   document.body.appendChild(textArea);
+
+//   textArea.select();
+//   document.execCommand("copy");
+//   document.body.removeChild(textArea);
+// });
+
+const fadeOutBackground = () => {
+  let bgOpacity = 1;
+  const background = document.getElementById("intro");
+
+  const fadeOut = setInterval(() => {
+    if (bgOpacity > 0) {
+      bgOpacity -= 0.01;
+      background.style.opacity = bgOpacity;
+    } else {
+      clearInterval(fadeOut);
+      document.body.style.overflowY = "scroll";
+    }
+  }, 10);
+};
+
 const buka = async () => {
-  document.getElementById("daftar-ucapan").innerHTML = comment.renderLoading(
-    pagination.getPer()
-  );
-  document.querySelector("body").style.overflowY = "scroll";
-
-  opacity("welcome");
-  // document.getElementById("tombol-musik").style.display = "block";
-  AOS.init();
-  audio.play();
-
+  fadeOutBackground();
   await confetti({
     origin: { y: 0.8 },
     zIndex: 1057,
   });
   animation();
-
-  timer();
 };
 
-// salin
+const animation = () => {
+  const duration = 10 * 1000;
+  const animationEnd = Date.now() + duration;
+  let skew = 1;
 
-document.getElementById("copyButton").addEventListener("click", function () {
-  let nomor = this.getAttribute("data-nomer");
-  let textArea = document.createElement("textarea");
+  let randomInRange = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
 
-  textArea.value = nomor;
-  document.body.appendChild(textArea);
+  (function frame() {
+    const timeLeft = animationEnd - Date.now();
+    const ticks = Math.max(200, 500 * (timeLeft / duration));
 
-  textArea.select();
-  document.execCommand("copy");
-  document.body.removeChild(textArea);
-});
+    skew = Math.max(0.8, skew - 0.001);
 
-// -------------------- Reload ke menu home ---------------------//
-document.addEventListener("DOMContentLoaded", function () {
-  window.scrollTo({
-    top: document.querySelector("main #welcome").offsetTop,
-    behavior: "smooth",
-  });
-});
+    confetti({
+      particleCount: 1,
+      startVelocity: 0,
+      ticks: ticks,
+      origin: {
+        x: Math.random(),
+        y: Math.random() * skew - 0.2,
+      },
+      colors: ["FFC0CB", "FF69B4", "FF1493", "C71585"],
+      shapes: ["heart"],
+      gravity: randomInRange(0.5, 1),
+      scalar: randomInRange(1, 2),
+      drift: randomInRange(-0.5, 0.5),
+    });
 
-// -------------------- Reload ke menu home ---------------------//
+    if (timeLeft > 0) {
+      requestAnimationFrame(frame);
+    }
+  })();
+};
